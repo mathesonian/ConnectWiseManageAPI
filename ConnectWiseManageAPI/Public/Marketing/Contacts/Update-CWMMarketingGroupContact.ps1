95622d3dae71c309b36e2a9cdef86ce21423de1e
Chris <christopher.matheson@gmail.com>
2022-08-05 16:00:07 -0500

    Create Update-CWMMarketingGroupContact.ps1

---
+++ ConnectWiseManageAPI/Public/Marketing/Contacts/Update-CWMMarketingGroupContact.ps1
@@ -0,0 +1,13 @@
+function Update-CWMMarketingGroupContact {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [int]$id
+        [Alias('contactId')]
+        [int]$parentId,
+        [Alias('groupId')]
+    )
+
+    $Endpoint = "/marketing/groups/$($parentId)/contacts/$($id)"
+    Invoke-CWMUpdateMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
