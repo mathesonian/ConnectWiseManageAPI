03087fc7131ca18b7561d2ec8fc52a8910653c6e
Chris <christopher.matheson@gmail.com>
2022-08-08 15:39:24 -0500

    Update New-CWMMarketingGroupContact.ps1

--- ConnectWiseManageAPI/Public/Marketing/Contacts/New-CWMMarketingGroupContact.ps1
+++ ConnectWiseManageAPI/Public/Marketing/Contacts/New-CWMMarketingGroupContact.ps1
@@ -2,7 +2,7 @@ function New-CWMMarketingGroupContact {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
-        [int]$id
+        [int]$id,
         [Alias('contactId')]
         [int]$parentId,
         [Alias('groupId')]
